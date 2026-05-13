70e764d2c69cd925b239e79fecd3523ac77240f6
unknown <reppollio@gmail.com>
2026-05-13 16:44:16 -0500

    refactor: updated content of routes to add iam routes.

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from './shared/presentation/views/home.vue'
 import PageNotFoundView from './shared/presentation/views/page-not-found.vue'
+import iamRoutes from './iam/presentation/iam-routes.js'
 
 const routes = [
     { path: '/', redirect: '/home' },
     { path: '/home', name: 'home', component: HomeView },
+    ...iamRoutes,
     { path: '/:pathMatch(.*)*', name: 'not-found', component: PageNotFoundView }
 ]
 
