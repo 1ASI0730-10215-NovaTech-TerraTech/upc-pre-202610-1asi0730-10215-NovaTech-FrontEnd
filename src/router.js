4adafbd696464e1b6405c85b39799d330e4eb8e5
breithner1911 <breithner1911@gmail.com>
2026-05-13 17:57:32 -0500

    feat(router): enhance routing with profile management and monitoring routes.

--- src/router.js
+++ src/router.js
@@ -1,11 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouterView } from 'vue-router'
 import HomeView from './shared/presentation/views/home.vue'
 import PageNotFoundView from './shared/presentation/views/page-not-found.vue'
+import iamRoutes from './iam/presentation/iam-routes.js'
+import monitoringRoutes from './monitoring/presentation/monitoring-routes.js'
+import {commercialRoutes} from "./commercial/presentation/commercial-management-routes.js";
+import profileManagementRoutes from "./profile-management/presentation/profile-management-routes.js";
 
 const routes = [
     { path: '/', redirect: '/home' },
     { path: '/home', name: 'home', component: HomeView },
-    { path: '/:pathMatch(.*)*', name: 'not-found', component: PageNotFoundView }
+    ...iamRoutes,
+    { path: '/monitoring', component: RouterView, children: monitoringRoutes },
+    { path: '/profile',         name: 'profile-management', children: profileManagementRoutes },
+    { path: '/commercial', component: RouterView, children: commercialRoutes },
+    { path: '/:pathMatch(.)', name: 'not-found', component: PageNotFoundView }
 ]
 
 const router = createRouter({
@@ -14,3 +22,6 @@ const router = createRouter({
 })
 
 export default router
+
+
+
