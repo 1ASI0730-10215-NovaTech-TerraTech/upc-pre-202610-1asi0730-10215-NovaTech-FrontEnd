6d086a9612e03af6c55c3dc435fa074ed8c681a4
Guillermo Howard <u202222275@upc.edu.pe>
2026-05-13 14:13:40 -0500

    feat(monitoring): add device assembler for transforming device resources to entities.

---
+++ src/monitoring/infrastructure/device.assembler.js
@@ -0,0 +1,18 @@
+import {Device} from "../domain/model/device.entity.js";
+
+export class DeviceAssembler {
+
+    static toEntityFromResource(resource) {
+        return new Device({...resource});
+    }
+
+    static toEntitiesFromResponse(response) {
+        if (response.status !== 200) {
+            console.error(`${response.status} - ${response.statusText}`);
+            return [];
+        }
+        let resources = response.data instanceof Array ? response.data
+            : response.data['device'];
+        return resources.map(resource => this.toEntityFromResource(resource));
+    }
+}
