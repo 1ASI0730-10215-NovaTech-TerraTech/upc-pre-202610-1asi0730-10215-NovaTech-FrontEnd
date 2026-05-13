b086c4d1c3ecffdb6f5d5850e8d4213a13b5257e
Rodri2712 <rodri.aguilar.2005@gmail.com>
2026-05-13 15:44:34 -0500

    feat(stock): update router with stock view

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from './shared/presentation/views/home.vue'
 import PageNotFoundView from './shared/presentation/views/page-not-found.vue'
-import InventoryView from './inventory/presentation/views/inventory.vue'
+import StockView from './stock/presentation/views/stock-view.vue'
 
 const routes = [
     { path: '/', redirect: '/home' },
     { path: '/home', name: 'home', component: HomeView },
-    { path: '/inventory', name: 'inventory', component: InventoryView },
+    { path: '/inventory', name: 'inventory', component: StockView },
     { path: '/:pathMatch(.*)*', name: 'not-found', component: PageNotFoundView }
 ]
 
