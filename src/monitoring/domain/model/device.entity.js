6b719818979d219792a6f7fcfdefc11c04e6efe0
GuillermoPromac <u202222275@upc.edu.pe>
2026-07-01 17:02:45 -0500

    feat(monitoring): enhance device and field classes with detailed jsdoc comments.

--- src/monitoring/domain/model/device.entity.js
+++ src/monitoring/domain/model/device.entity.js
@@ -1,3 +1,12 @@
+/**
+ * Represents an IoT monitoring device.
+ * @class Device
+ * @property {number} id - Unique device identifier.
+ * @property {number} field_id - ID of the associated field.
+ * @property {string} mac_address - MAC address of the device.
+ * @property {string} status - Current status: 'ONLINE', 'OFFLINE', or 'LOW_BATTERY'.
+ * @property {string} last_sync - Timestamp of the last synchronization.
+ */
 export class Device {
     id;
     field_id;
@@ -5,6 +14,15 @@ export class Device {
     status;
     last_sync;
 
+    /**
+     * Creates a new Device instance.
+     * @param {Object} params - Device parameters.
+     * @param {number} [params.id=0] - Device ID.
+     * @param {number} [params.field_id=0] - Associated field ID.
+     * @param {string} [params.mac_address=''] - MAC address.
+     * @param {string} [params.status=''] - Device status.
+     * @param {string} [params.last_sync=''] - Last synchronization timestamp.
+     */
     constructor({ id = 0, field_id = 0, mac_address = '', status = '', last_sync = '' }) {
         this.id = id;
         this.field_id = field_id;
--- src/monitoring/domain/model/field.entity.js
+++ src/monitoring/domain/model/field.entity.js
@@ -1,20 +1,35 @@
+/**
+ * Represents an agricultural field.
+ * @class Field
+ * @property {number} id - Unique field identifier.
+ * @property {number} profile_id - ID of the owning profile.
+ * @property {string} name - Field name.
+ * @property {number} size_m2 - Size in square meters.
+ * @property {string} soil_type - Type of soil (e.g., 'Sandy', 'Clay-loam').
+ * @property {number} latitude - Latitude coordinate.
+ * @property {number} longitude - Longitude coordinate.
+ */
 export class Field {
-
     id;
-
     profile_id;
-
     name;
-
     size_m2;
-
     soil_type;
-
     latitude;
-
     longitude;
 
-    constructor({ id = 0, profile_id = 0, name = '', size_m2 = 0, soil_type = '', latitude = 0, longitude = 0}) {
+    /**
+     * Creates a new Field instance.
+     * @param {Object} params - Field parameters.
+     * @param {number} [params.id=0] - Field ID.
+     * @param {number} [params.profile_id=0] - Profile ID.
+     * @param {string} [params.name=''] - Field name.
+     * @param {number} [params.size_m2=0] - Size in square meters.
+     * @param {string} [params.soil_type=''] - Soil type.
+     * @param {number} [params.latitude=0] - Latitude.
+     * @param {number} [params.longitude=0] - Longitude.
+     */
+    constructor({ id = 0, profile_id = 0, name = '', size_m2 = 0, soil_type = '', latitude = 0, longitude = 0 }) {
         this.id = id;
         this.profile_id = profile_id;
         this.name = name;
